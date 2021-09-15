6c9ddd2af51f807f8007337f0f463b2932bd7d61
mazenmohamed5 <88230604+mazenmohamed5@users.noreply.github.com>
2021-09-15 18:05:43 +0200

    Add files via upload

---
+++ oop8 user define constructor.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class mother
+{
+public:
+	mother()
+
+	{
+		cout << "default constructor of mother" << endl;
+	}
+	mother(int a)
+	{
+		cout << "user define constructor of mother" << endl;
+	}
+
+};
+class son : public mother
+{
+public:
+	son(int a) :mother(5)
+	{
+		cout << "user define constructor of son" << endl;
+	}
+};
+
+void main()
+{
+	son ahmed(50);
+}
